16ca8f1937c389e48a9e8f3177ce136943ae0914
Eric Schikschneit <eschik123@yahoo.com>
2023-08-18 23:34:25 -0500

    imagetest: fix bug where UI hangs
    Signed-off-by: Eric Schikschneit <eschik123@yahoo.com>

--- consoleevent.cpp
+++ consoleevent.cpp
@@ -8,8 +8,8 @@ consoleevent::consoleevent(QObject *parent) : QObject(parent)
 
 void consoleevent::runCommand() {
     QProcess cmdProcess;
-    cmdProcess.start("/bin/bash -c md5sum /tmp/img.png");
-    cmdProcess.waitForFinished(-1); //Will hang here waiting to finish, should be quick
+    cmdProcess.start("md5sum /tmp/img.png");
+    cmdProcess.waitForFinished(300); //Will hang here waiting to finish, should be quick
     stdOutStr = cmdProcess.readAllStandardOutput();
     stdErrStr = cmdProcess.readAllStandardError();
     emit commandComplete();
